64c44c338cc26cae4e3c38f82761c86e78e56bfd
rakshya01 <kunwarrakshya20@gmail.com>
2025-04-11 10:07:49 +0545

    adding

--- src/Auth/ProtectedRoutes.jsx
+++ src/Auth/ProtectedRoutes.jsx
@@ -1,18 +1,25 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function ProtectedRoutes({components}) {
 
-    const { user, isAuthenticated} = useAuth0();
+    const {  isAuthenticated, isLoading} = useAuth0();
 
     const navigate = useNavigate()
 
-    if(!isAuthenticated) {
-        return navigate("/login")
-    }
 
-  return components;
+    useEffect(()=>{
+      if(!isLoading && !isAuthenticated){
+        navigate("/login");
+
+      }
+    },[isAuthenticated, isLoading,navigate])
+    
+    if(isLoading) return <div>Loading.....</div>
+
+  return isAuthenticated ? components : null;
+
 }
 
 export default ProtectedRoutes
