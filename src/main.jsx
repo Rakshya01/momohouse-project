f0967e90c3f758bf1da162cf5ad3fa959fbc7860
rakshya01 <kunwarrakshya20@gmail.com>
2025-04-11 10:31:58 +0545

    adding

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './Context/CartProvider.jsx'
-import { Bounce, ToastContainer } from 'react-toastify'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./Context/CartProvider.jsx";
+import { Bounce, ToastContainer } from "react-toastify";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <CartProvider>
+    <CartProvider>
+      <Auth0Provider
+        domain="dev-vwej160uu55axhgf.us.auth0.com"
+        clientId="KED3hSMZAtmeCc7iULBa8jVSkJ0IYd5y"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
 
-  <Auth0Provider
-    domain="dev-vwej160uu55axhgf.us.auth0.com"
-    clientId="KED3hSMZAtmeCc7iULBa8jVSkJ0IYd5y"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>,
-    <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick={false}
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-transition={Bounce}
-/>
-  </CartProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </CartProvider>
   </BrowserRouter>
-
-)
+);
--- src/payment/Failure.jsx
+++ src/payment/Failure.jsx
@@ -1,34 +1,28 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom';
+// import React from "react";
+// import { useSearchParams } from "react-router-dom";
 
 function Failure() {
+  // const [searchParams] = useSearchParams();
 
-  const [searchParams] = useSearchParams()
-      let val = searchParams.get("data");
-      console.log(val);
-
-
-      let info = atob(val);
-      info= JSON.parse(info);
-      console.log(info);
   return (
     <div>
-       <div className='pt-20'>
-      <div className='shadow-2xl space-y-2 shadow-gray-400 w-96 m-auto mt-5 flex flex-col items-center p-5'>
+      <div className="pt-20">
+        <div className="shadow-2xl space-y-2 shadow-gray-400 w-96 m-auto mt-5 flex flex-col items-center p-5">
+          <img
+            className="h-44"
+            src="https://cdn.printme.online/wp-content/uploads/2020/04/payment_fail_icon.png"
+          />
 
-    <img className='h-44' src="https://cdn.printme.online/wp-content/uploads/2020/04/payment_fail_icon.png"/>
-
-
-    <div>
-      <h1>Status: <span className='underline bg-green-300'>{info?.status}</span></h1>
-    </div>
-
+          <div>
+            <h1>
+              Status:Fail
+              <span className="underline bg-green-300">{info?.status}</span>
+            </h1>
+          </div>
+        </div>
       </div>
-
-     
     </div>
-    </div>
-  )
+  );
 }
 
-export default Failure
+export default Failure;
